349dac794499f373a2bf70bec10e477d6548c3e2
Saidamir <saidamirhoja2015@mail.ru>
2021-04-28 14:18:42 +0500

    completed

---
+++ resources/views/blog/post.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+
+@section('content')
+<div class="row">
+    <div class="col-md-12 text-center">
+        <h1 class="quote">Learning Laravel</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis delectus, enim, eos ex explicabo fugit harum labore perferendis quo sunt totam veniam voluptate. Inventore necessitatibus repellat rerum sapiente unde.</p>
+        <p><a href="#">Read more...</a></p>
+    </div>
+</div>
+@endsection
---
+++ resources/views/other/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">About Me</p>
+        </div>
+    </div>
+    <div class="row">
+        <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus adipisci, asperiores assumenda beatae dicta dolores dolorum eligendi esse ex excepturi facere harum ipsa libero maiores molestiae repellendus veniam voluptates.</span><span>At debitis dicta enim non quis reiciendis rem vero. Beatae debitis ipsam, iure minima nemo nulla possimus quasi recusandae rerum suscipit? A consequuntur, corporis eveniet ipsum molestias omnis tempora tempore!</span><span>Adipisci aliquam animi autem culpa debitis, dolores enim eveniet facere incidunt, laudantium minus molestias necessitatibus nesciunt nobis optio quaerat quam sint veritatis vero voluptates. Exercitationem, magni repudiandae? Explicabo, laboriosam maxime.</span></p>
+    </div>
+    <hr>
+
+@endsection
